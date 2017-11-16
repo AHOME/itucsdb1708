0656a9ffaab4b7bd0302d81983aaaa31865c8680
onatsahin <onat@Onats-MacBook-Pro.local>
2017-11-16 22:33:49 +0300

    Created classes for deals and orders

---
+++ order.py
@@ -0,0 +1,15 @@
+#from server import app
+import psycopg2 as dbapi2
+from flask import current_app
+from flask_login import UserMixin
+from passlib.apps import custom_app_context as pwd_context
+#from passlib.ext.django.models import password_context
+
+class Order(UserMixin):
+    def __init__(self, primaryId, userId, restaurantId, price, date, status):
+        self.primaryId = primaryId
+        self.userId = userId
+        self.restaurantId = restaurantId
+        self.price = price
+        self.date = date
+        self.status = status
