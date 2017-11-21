c28757b4838e01fa811f8eb118a98d8fd4a608ae
Alpkant <kantarcia@itu.edu.tr>
2017-11-21 12:45:58 +0300

    General fixes for errors in the database

---
+++ classes/drinks.py
@@ -0,0 +1,13 @@
+import psycopg2 as dbapi2
+from flask import current_app
+from flask_login import UserMixin
+
+
+class Drinks():
+    def __init__(self, Id, name, drinkType, calorie, drinkCold ,alcohol):
+        self.Id = Id
+        self.name = name
+        self.drinkType = drinkType
+        self.calorie = calorie
+        self.drinkCold = drinkCold
+        self.alcohol = alcohol
---
+++ classes/restaurant_foods.py
@@ -0,0 +1,12 @@
+#from server import app
+import psycopg2 as dbapi2
+from flask import current_app
+from flask_login import UserMixin
+from passlib.apps import custom_app_context as pwd_context
+#from passlib.ext.django.models import password_context
+
+class RestaurantFoods():
+    def __init__(self, primaryId, restaurantId, foodId):
+        self.primaryId = primaryId
+        self.restaurantId = restaurantId
+        self.foodId = foodId
